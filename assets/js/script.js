787ca76d665f3bba1288d03385cbaa58f13d700b
xtrp <36894883+xtrp@users.noreply.github.com>
2018-06-14 18:47:01 +0100

    major updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 var templatesList = [
-    "Me"
+    ["Me","Me","#1F3845"]
 ];
 window.onload = function(){
     // Footer Copyright Year
@@ -7,5 +7,11 @@ window.onload = function(){
         let d = new Date();
         document.querySelector("footer").innerHTML += d.getFullYear();
     })();
-        
+    
+    // Add Template Links
+    (function(){
+        templatesList.forEach(function(item,index){
+            document.querySelector("div.mainDownloadArea").innerHTML += "<div class='template' style='--main-color: " + item[2] + ";'><a class='imageLink' href='https://html5rocket.github.io/" + item[1] + "'><img src='assets/img/templateHeroImages/" + item[1] + ".png'></a><div class='mainArea'><h1>" + item[0] + "</h1><ul class='links'><a class='demo' href='https://html5rocket.github.io/" + item[1] + "'>Live Demo</a><a href='assets/downloads/" + item[1] + ".zip' download>Free Download</a></ul></div></div>";
+        });
+    })();
 }
