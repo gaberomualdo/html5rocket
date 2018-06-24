3711cf9d5b6d95fa5e13913856b96cdf61df3ae7
xtrp <36894883+xtrp@users.noreply.github.com>
2018-06-24 19:38:06 +0100

    cleaned up script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,33 @@
 var templatesList = [
-    ["Me","Me","#1F3845"],
-    ["Soon","Soon","#0D1E35"],
-    ["Carbon","Carbon","#0F0F0E"]
+  ["Me", "Me", "#1F3845"],
+  ["Soon", "Soon", "#0D1E35"],
+  ["Carbon", "Carbon", "#0F0F0E"]
 ];
-window.onload = function(){
-    // Footer Copyright Year
-    (function(){
-        let d = new Date();
-        document.querySelector("footer").innerHTML += d.getFullYear();
-    })();
-    
-    // Add Template Links
-    (function(){
-        templatesList.forEach(function(item,index){
-            document.querySelector("div.mainDownloadArea").innerHTML += "<div class='template' style='--main-color: " + item[2] + ";'><a class='imageLink' href='https://html5rocket.github.io/" + item[1] + "'><img src='assets/img/templateHeroImages/" + item[1] + ".png'></a><div class='mainArea'><h1>" + item[0] + "</h1><ul class='links'><a class='demo' href='https://html5rocket.github.io/" + item[1] + "'>Live Demo</a><a href='assets/downloads/" + item[1] + ".zip' download><span>Free</span> Download</a></ul></div></div>";
-        });
-    })();
-}
+templatesList.reverse();
+window.onload = function() {
+  // Footer Copyright Year
+  (function() {
+    let d = new Date();
+    document.querySelector("footer").innerHTML += d.getFullYear();
+  })();
+
+  // Add Template Links
+  (function() {
+    templatesList.forEach(function(item, index) {
+      document.querySelector("div.mainDownloadArea").innerHTML +=
+        "<div class='template' style='--main-color: " +
+        item[2] +
+        ";'><a class='imageLink' href='https://html5rocket.github.io/" +
+        item[1] +
+        "'><img src='assets/img/templateHeroImages/" +
+        item[1] +
+        ".png'></a><div class='mainArea'><h1>" +
+        item[0] +
+        "</h1><ul class='links'><a class='demo' href='https://html5rocket.github.io/" +
+        item[1] +
+        "'>Live Demo</a><a href='assets/downloads/" +
+        item[1] +
+        ".zip' download><span>Free</span> Download</a></ul></div></div>";
+    });
+  })();
+};
